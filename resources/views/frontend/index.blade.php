8a0779ac5360e140351d6a28250692049eafc0b1
darshan45672 <drshnbhandary@gmail.com>
2024-12-08 20:10:08 +0530

    done

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.front')
 
 @section('content')
-<div><h1>jai<br> shree ram</h1></div>
-<header class="{{ $headerClass=" video" }}">
+{{-- <div><h1>jai<br> shree ram</h1></div> --}}
+<header class="{{ $headerClass=" video" }} mt-4">
 
   <!-- This div is  intentionally blank. It creates the transparent black overlay over the video which you can modify in the CSS -->
   <div class="overlay"></div>
